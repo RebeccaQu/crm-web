1397c07e9e84ccdcc2054131399693ca087a05d2
Rebecca Qu <beks.qu@gmail.com>
2015-06-16 16:17:23 -0400

    fixed bugs

--- crm.rb
+++ crm.rb
@@ -17,7 +17,12 @@ get '/contacts/new' do
 	erb :new_contact
 end
 
+# get '/contacts/:id' do
+# params[:id]
+# end
+
 post '/contacts' do
-	new_contact = Contact.new(params[:first_name], params[:last_name], params[$rolodex.add_contact(new_contact)])
-	redirect to('/contacts')
+	new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:notes])
+		$rolodex.add_contact(new_contact)
+		redirect to('/contacts')
 end
